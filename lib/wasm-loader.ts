68e78d831678d228b2922e06ddc282804c29e74e
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2026-06-19 16:06:35 +0530

    fix: cache WASM init() promise to prevent concurrent double-initialisation

--- lib/wasm-loader.ts
+++ lib/wasm-loader.ts
@@ -1,6 +1,11 @@
 import init, { BrowserDb } from '@minigraf/browser'
 
+let wasmInit: Promise<unknown> | null = null
+
 export async function loadMinigraf(dbName: string) {
-  await init()
+  if (!wasmInit) {
+    wasmInit = init()
+  }
+  await wasmInit
   return BrowserDb.open(dbName)
 }
